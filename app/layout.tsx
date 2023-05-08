6aff9a4307c544877fa12af716bb74f383a1be6e
thutasann <thutasann2002@gmail.com>
2023-05-08 16:49:28 +0630

    chore: clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Next Talk',
+export const metadata: Metadata = {
+  title: 'Nexenger',
   description: 'This is the RealTime Messenger using NEXT13 App Dir.',
+  icons: {
+    icon: '/nexenger.png',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>NEXT messenger</h1>
+  return <div>ok</div>
 }
