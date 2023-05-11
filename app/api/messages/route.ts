6e2605405578bfb9e352a6c9558469077f98181e
thutasann <thutasann2002@gmail.com>
2023-05-11 16:00:17 +0630

    fix: send message

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,31 +1,31 @@
 import getCurrentUser from '@/actions/getCurrentUser'
 import { NextResponse } from 'next/server'
 import { pusherServer } from '@/libs/pusher'
+import prisma from '@/libs/prismadb'
 
 export async function POST(request: Request) {
   try {
     const currentUser = await getCurrentUser()
-    const body: IMessagePayload = await request.json()
+    const body = await request.json()
     const { message, image, conversationId } = body
 
     if (!currentUser?.id || !currentUser?.email) {
-      return new NextResponse('Unauthorized', {
-        status: 401,
-      })
+      return new NextResponse('Unauthorized', { status: 401 })
     }
-    const newMessage = await prisma?.message.create({
+
+    const newMessage = await prisma.message.create({
+      include: {
+        seen: true,
+        sender: true,
+      },
       data: {
         body: message,
         image: image,
         conversation: {
-          connect: {
-            id: conversationId,
-          },
+          connect: { id: conversationId },
         },
         sender: {
-          connect: {
-            id: currentUser.id,
-          },
+          connect: { id: currentUser.id },
         },
         seen: {
           connect: {
@@ -33,13 +33,9 @@ export async function POST(request: Request) {
           },
         },
       },
-      include: {
-        seen: true,
-        sender: true,
-      },
     })
 
-    const updatedConversation = await prisma?.conversation.update({
+    const updatedConversation = await prisma.conversation.update({
       where: {
         id: conversationId,
       },
@@ -47,7 +43,7 @@ export async function POST(request: Request) {
         lastMessagAt: new Date(),
         messages: {
           connect: {
-            id: newMessage?.id,
+            id: newMessage.id,
           },
         },
       },
@@ -63,9 +59,9 @@ export async function POST(request: Request) {
 
     await pusherServer.trigger(conversationId, 'messages:new', newMessage)
 
-    const lastMessage = updatedConversation?.messages[updatedConversation.messages.length - 1]
+    const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1]
 
-    updatedConversation?.users.map(user => {
+    updatedConversation.users.map(user => {
       pusherServer.trigger(user.email!, 'conversation:update', {
         id: conversationId,
         messages: [lastMessage],
@@ -74,9 +70,7 @@ export async function POST(request: Request) {
 
     return NextResponse.json(newMessage)
   } catch (error) {
-    console.log('ERROR MESSAGE: ', error)
-    return new NextResponse('Internal Error ', {
-      status: 500,
-    })
+    console.log(error, 'ERROR_MESSAGES')
+    return new NextResponse('Error', { status: 500 })
   }
 }
